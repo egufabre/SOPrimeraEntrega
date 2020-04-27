29ec85176277601f2beb415cadba7c7f3c8bef80
leelleon <leonardosantosbran@gmail.com>
2020-04-27 02:03:20 -0300

    Bash para eliminar usuarios del sistema

---
+++ DelUser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+read -p "Ingrese el nombre del Usuario a borrar:_    "    USER
+UserName=$USER
+
+if sudo userdel -rf $UserName
+then
+    echo Usuario Eliminado con exito!
+else
+    echo El usuario no existe 
+
